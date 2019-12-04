437db7e73e4cdf00ac693d1a3b5a4e80cab0e4f4
Jennifer McMullen <jmcmul@umich.edu>
2019-12-04 16:48:29 -0500

    changes

--- billboard_API.py
+++ billboard_API.py
@@ -8,18 +8,18 @@ chart = billboard.ChartData('hot-100', date='2008-12-31')
 song_number1 = chart[0]
 song_number2 = chart[1]
 
-top_20 = chart[:20]
+top_50 = chart[:50]
 
-top_20_song_artists = []
-for entry in top_20:
-    top_20_song_artists.append(entry.artist)
+top_50_song_artists = []
+for entry in top_50:
+    top_50_song_artists.append(entry.artist)
 
-top_20_song_names = []
-for song in top_20:
-    top_20_song_names.append(song.title)
+top_50_song_names = []
+for song in top_50:
+    top_50_song_names.append(song.title)
 
 list_of_urls = []
-for (x,y) in zip(top_20_song_names, top_20_song_artists):
+for (x,y) in zip(top_50_song_names, top_50_song_artists):
     search = "{} {}".format(x,y)
     results = wikipedia.search(search,results=2)
     searched = wikipedia.page(results[0])
@@ -43,11 +43,8 @@ for url in list_of_urls:
         genres.append(genre)
         #print(genre) 
 
-#print(genres)
-#print('\n')
-#print(top_20_song_names)
 
-genres_names_list = tuple(zip(top_20_song_names, genres))
+genres_names_list = tuple(zip(top_50_song_names, genres))
 print(genres_names_list)
 
 
