c3eae4883a7192eddf7c8add169785b072c2e497
Edwin Mui <Edwin@0587380799.wireless.umich.net>
2019-12-06 16:11:00 -0500

    heavy work on spotify

--- spotify.py
+++ spotify.py
@@ -3,6 +3,8 @@ import spotify_info
 import spotipy.util as util
 import matplotlib
 import matplotlib.pyplot as plt 
+import wikipedia
+from bs4 import BeautifulSoup
 
 import sys
 import os
@@ -11,94 +13,120 @@ import json
 import sqlite3
 import webbrowser
 import urllib.request, urllib.parse, urllib.error
+from json.decoder import JSONDecodeError
 
 
+# HOW TO RUN: 
+# if you want ot check out a different user, just change "username"
 
-#HOW TO RUN: 
-#run the code, and you will get redirected to google. enter the google URL into terminal and click enter
-#after you do that, run the code again and you will be all set
-
+# gets spotify's authorization info
 clientid = spotify_info.consumer_key
 clientid_secret = spotify_info.secret_key
 redirecturi = spotify_info.redirect_uri
 
-#get the username from terminal
+# user ID: 1294124325
 username = '1294124325'
 scope = 'user-library-read'
+
+# erase cache and prompt for user permission
 try: 
-    token = util.prompt_for_user_token(username, scope,client_id=clientid, client_secret= clientid_secret, redirect_uri= redirecturi)
+    token = util.prompt_for_user_token(username,scope,client_id= clientid,client_secret=clientid_secret,redirect_uri=redirecturi)
 
 except: 
     os.remove(f'.cache-{username}')
-    token = util.prompt_for_user_token(username, scope,client_id=clientid, client_secret= clientid_secret, redirect_uri= redirecturi)
+    token = util.prompt_for_user_token(username,scope,client_id=clientid,client_secret=clientid_secret,redirect_uri=redirecturi)
 
-
-
-#token = util.prompt_for_user_token(username, scope,client_id=clientid, client_secret= clientid_secret, redirect_uri= redirecturi)
-#creating spotify object
+# creates spotify object
 spotify_object = spotipy.Spotify(auth=token)
 
-#gets content from the current spotify user
+# gets content from the current spotify user
 user = spotify_object.current_user()
-#print(json.dumps(user, sort_keys=True, indent=4))
+
+
 
 #gets current users playlists
 results = spotify_object.current_user_playlists()
-#creates a list of playlist ID numbers
-playlist_list = []
-playlist_name_list = []
-playlist_id_name = {}
-# playlist_list is a list of the playlist IDs --> prints [3423sdf, 34230ow, 234234k]
-# playlist_name_list is a list of the actual playlist names --> prints ['Party!', 'SI 206', 'edwin']
-# playlist_id_name is a dictionary where the keys are the ID name and the values are the playlist name
-for x in results['items']:
-    playlist_name = x["id"]
-    my_playlist_names = x['name']
 
-    playlist_list.append(playlist_name)
-    playlist_name_list.append(my_playlist_names)
+# a list of the playlist IDs --> prints [3423sdf, 34230ow, 234234k]
+playlist_list_ids = []
+# a list of the actual playlist names --> prints ['Party!', 'SI 206', 'edwin']
+playlist_names = []
+# a dictionary where the keys are the ID name and the values are the playlist name
+playlist_ids_and_names = {}
 
-    playlist_id_name[playlist_name] = my_playlist_names
+for item in results['items']:
+    playlist_id = item["id"]
+    playlist_name = item['name']
 
-# makes a dictionary where the key is the ID number and the value are all the songs in the playlist
-playlist_result_dict = {}
-playlist_count = 0
-for x in playlist_list:
-    playlist_result_dict[x] = spotify_object.user_playlist(username, playlist_id = x)
-    #new_results = spotify_object.user_playlist(username, x)
+    playlist_list_ids.append(playlist_id)
+    playlist_names.append(playlist_names)
 
-#gets all the individual songs in the playlist and adds them to a dictionary 
-songs_in_playlist = {}
-for playlist_id in playlist_result_dict.keys():
-    song_list = []
-    artist_list = []
-    song_plus_artist = []
-    for y in playlist_result_dict[playlist_id]["tracks"]['items']:
-        song_name = y['track']['name']
-        song_list.append(song_name)
-        artist_name = y['track']['album']['artists']
-        for artist in artist_name:
-            actual_artist = artist['name']
-            artist_list.append(actual_artist)
-    for x in range(len(song_list)):
-        song_plus_artist.append((song_list[x], artist_list[x]))
-    songs_in_playlist[playlist_id] = song_plus_artist
-    print(songs_in_playlist)
+    playlist_ids_and_names[playlist_id] = playlist_name
 
-try:
-    filename = open('spotify.json', 'r')
-    file_results = json.loads(filename)
-    filename.close()
-    filename = open('spotify.json', 'w')
-    for x in playlist_result_dict.keys():
-        if x not in file_result.keys():
-            filename.write(json.dumps(playlist_result_dict[x]))
+# gets tracks of top 50 hits from 2013
+for playlist in playlist_ids_and_names.items():
+    if playlist[1] == "Top 50 Hits of 2013":
 
-except:
-    filename = open('spotify.json', 'w')
-    filename.write(json.dumps(playlist_result_dict))
+        # creates a spotify object with id
+        playlist_object = spotify_object.user_playlist(username, playlist_id=playlist[0])
 
-# CREATING A DATABASE
+        # gets all individuals songs
+        songs_and_artists = []
+        # loops through all tracks and adds information to lists
+        for track in playlist_object["tracks"]["items"]:
+            # helps insert key, value pairs into songs and artists dictionary
+            song_list = []
+            artist_list = []
+
+            # gets song name
+            song_name = track["track"]["name"]
+            song_list.append(song_name)
+
+            # gets all artist information
+            artist_name = track["track"]["album"]["artists"]
+            # finds the actual artist
+            for artist in artist_name:
+                actual_artist_name = artist["name"]
+                artist_list.append(actual_artist_name)
+
+            # adds both song and artist in tuple form to the list songs_and_artists
+            songs_and_artists.append((song_list[0], artist_list[0]))
+
+# print(songs_and_artists)                  DELETE THIS LATER!!
+
+# searches up songs and artists to identify genres
+list_of_urls = []
+# songs_and_artists is a list of track tuples containing the song and artist
+for track in songs_and_artists:
+    search = "{} {}".format(track[0],track[1])
+    search_results = wikipedia.search(search, results = 2)
+    searched = wikipedia.page(search_results[0])
+    list_of_urls.append(searched.url)
+
+# gets all genres
+genres = []
+# iterates through urls to find genre within page of text
+for url in list_of_urls:
+    soup = BeautifulSoup(requests.get(url).text, "html.parser")
+    table = soup.find("table", {"class": "infobox"})
+    list_of_genres1 = table.find("td", {"class": "category hlist"})
+    if list_of_genres1 != None:
+        if list_of_genres1.find("a") != None:
+            genre = list_of_genres1.find("a")
+            genres.append(genre.text)
+        else:
+            list_of_genres2 = list_of_genres1.find("div", {"class": "hlist hlist-separated"})
+            li = list_of_genres1.find("ul")
+            genre = li.find("a")
+            genres.append(genre.text)
+    else:
+        genre = "unknown"
+        genres.append(genre)
+
+print(genres)
+    
+
+# connect to database
 conn = sqlite3.connect('SIProject.sqlite')
 cur = conn.cursor()
 
@@ -110,8 +138,6 @@ for (key, val) in songs_in_playlist.items():
         cur.execute('INSERT INTO Spotify (playlist_name,playlist_id, song, artist) VALUES (?,?,?,?)', (playlist_id_name[key],key, song[0], song[1]))
 conn.commit()
 conn.close()
-
-
 
 
 
--- spotify_info.py
+++ spotify_info.py
@@ -1,4 +1,4 @@
 # everything that Spotify needs to run
 consumer_key = 'a5388973ccce4eb383dae5a5e699f6af'
 secret_key = 'ff78157adb534c14951c00c498b00d41'
-redirect_uri = 'si206finalproject://returnafterlogin'
+redirect_uri = 'https://google.com/'
